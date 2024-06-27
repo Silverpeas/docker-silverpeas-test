fed940f34ade0f10178fcc36bc1214bf744f0f9e
Miguel Moquillon <miguel.moquillon@silverpeas.com>
2024-06-27 15:34:26 +0200

    Add the migration of Jackrabbit to Oak

--- src/bin/setup.sh
+++ src/bin/setup.sh
@@ -1,8 +1,37 @@
 #!/usr/bin/env bash
 
+migrate_jcr() {
+  # figure out the data home directory (by default it is into the Silverpeas home directory)
+  data_home=`grep "SILVERPEAS_DATA_HOME[ ]*=" "${SILVERPEAS_HOME}/configuration/config.properties"  | cut -d '=' -f 2 | xargs`
+  if [ "Z${data_home}" = "Z" ]; then
+    data_home="${SILVERPEAS_HOME}/data"
+  else
+    data_home=`sed -e "s/{env./{/g" <<< "${data_home}"`
+    data_home=`eval echo -e "${data_home}"`
+  fi
+
+  # figure out now the JCR home directory (by default it is located into the data home directory)
+  jcr_home=`grep "JCR_HOME[ ]*=" "${SILVERPEAS_HOME}/configuration/config.properties"  | cut -d '=' -f 2 | xargs`
+  if [ "Z${jcr_home}" = "Z" ]; then
+    jcr_home="${data_home}/jcr"
+  else    
+    jcr_home=`sed -e "s/SILVERPEAS_DATA_HOME/data_home/g" <<< "${jcr_home}"`
+    jcr_home=`eval echo -e "${jcr_home}"`
+  fi
+
+  jcr_dir=`dirname ${jcr_home}`
+  if [ -d "${jcr_dir}/jackrabbit" ] && [ ! -d "${jcr_dir}/jcr/segmentstore" ]; then
+    echo "Migrate the JCR from Apache Jackrabbit 2 to Apache Jackrabbit Oak..."
+    if ! /opt/oak-migration/oak-migrate.sh "$jcr_dir/jackrabbit" "$jcr_dir/jcr"; then
+      exit 1
+    fi
+  fi
+}
+
 echo "Installation of Silverpeas $1"
 ./h2database start
 ./silverpeas clean install
+migrate_jcr
 ret=$?
 if [ $ret -eq 0 ]; then
    rm ../log/build-*
--- src/run.sh
+++ src/run.sh
@@ -11,18 +11,18 @@ configure_silverpeas() {
   local configure=0
   if [ "Z${locale}" != "Z" ]; then
     echo " -> set locale to ${locale}"
-    echo "SILVERPEAS_USER_LANGUAGE=${locale}" >> ${SILVERPEAS_HOME}/configuration/config.properties
-    echo "SILVERPEAS_CONTENT_LANGUAGES=${locale}" >> ${SILVERPEAS_HOME}/configuration/config.properties
+    echo "SILVERPEAS_USER_LANGUAGE=${locale}" >> "${SILVERPEAS_HOME}"/configuration/config.properties
+    echo "SILVERPEAS_CONTENT_LANGUAGES=${locale}" >> "${SILVERPEAS_HOME}"/configuration/config.properties
     configure=1
   fi
-  if [ -f ${SILVERPEAS_HOME}/configuration/custom_config.properties ]; then
+  if [ -f "${SILVERPEAS_HOME}/configuration/custom_config.properties" ]; then
     echo " -> set custom configuration from custom_config.properties"
-    sed -e "s/DB_.*$//g" ${SILVERPEAS_HOME}/configuration/custom_config.properties > ${SILVERPEAS_HOME}/configuration/custom_config.fixed.properties
-    cat ${SILVERPEAS_HOME}/configuration/custom_config.fixed.properties >> ${SILVERPEAS_HOME}/configuration/config.properties
-    rm ${SILVERPEAS_HOME}/configuration/custom_config.fixed.properties
+    sed -e "s/DB_.*$//g" "${SILVERPEAS_HOME}"/configuration/custom_config.properties > "${SILVERPEAS_HOME}"/configuration/custom_config.fixed.properties
+    cat "${SILVERPEAS_HOME}"/configuration/custom_config.fixed.properties >> "${SILVERPEAS_HOME}"/configuration/config.properties
+    rm "${SILVERPEAS_HOME}"/configuration/custom_config.fixed.properties
     configure=1
   fi
-  if [ -f ${SILVERPEAS_HOME}/configuration/silverpeas/CustomerSettings.xml ] || [ -f ${SILVERPEAS_HOME}/configuration/silverpeas/CustomSettings.xml ]; then
+  if [ -f "${SILVERPEAS_HOME}/configuration/silverpeas/CustomerSettings.xml" ] || [ -f "${SILVERPEAS_HOME}/configuration/silverpeas/CustomSettings.xml" ]; then
     echo " -> custom settings detected in CustomerSettings.xml or CustomSettings.xml"
     configure=1
   fi
@@ -32,10 +32,10 @@ configure_silverpeas() {
 }
 
 start_silverpeas() {
-  ${SILVERPEAS_HOME}/bin/h2database start
+  "${SILVERPEAS_HOME}"/bin/h2database start
 
   echo "Start Silverpeas..."
-  ${JBOSS_HOME}/bin/standalone.sh -b 0.0.0.0 -c standalone-full.xml &
+  "${JBOSS_HOME}"/bin/standalone.sh -b 0.0.0.0 -c standalone-full.xml &
   local pids=`jobs -p`
   wait $pids
 }
@@ -48,17 +48,17 @@ stop_silverpeas() {
     kill $pids &> /dev/null
   fi
   
-  ${SILVERPEAS_HOME}/bin/h2database stop
+  "${SILVERPEAS_HOME}"/bin/h2database stop
 }
 
 trap 'stop_silverpeas' SIGTERM
 
-if [ -f ${SILVERPEAS_HOME}/bin/.install ]; then
+if [ -f "${SILVERPEAS_HOME}/bin/.install" ]; then
   configure_silverpeas
-  rm ${SILVERPEAS_HOME}/bin/.install
+  rm "${SILVERPEAS_HOME}"/bin/.install
 fi
 
-if [ -f ${SILVERPEAS_HOME}/configuration/config.properties ] && [ ! -e ${SILVERPEAS_HOME}/bin/.install ]; then
+if [ -f "${SILVERPEAS_HOME}/configuration/config.properties" ] && [ ! -e "${SILVERPEAS_HOME}/bin/.install" ]; then
   start_silverpeas
 else
   echo "No ${SILVERPEAS_HOME}/configuration/config.properties found! No start!"
